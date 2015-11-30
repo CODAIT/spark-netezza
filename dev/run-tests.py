5e940a0e8ded6941269e4c86aec3081d70890152
Xiu Guo <xguo27@gmail.com>
2015-11-29 21:07:45 -0800

    Run unit test from dev/run-tests

--- dev/run-tests.py
+++ dev/run-tests.py
@@ -3,10 +3,16 @@ import os
 ROOT = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../"))
 
 def run_scala_style_checks():
+
   os.system(os.path.join(ROOT, "dev/lint-scala"))
+
+def run_scala_tests():
+
+  os.system(os.path.join(ROOT, "build/sbt test"))
 
 def main():
   run_scala_style_checks()
+  run_scala_tests()
 
 if __name__ == "__main__":
     main()
